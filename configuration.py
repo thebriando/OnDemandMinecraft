2e51c651c96e40b7383092ccd5e301d7228b8808
thebriando <thebriando@gmail.com>
2020-06-08 20:11:50 -0700

    setup ec2 and keypairs

--- configuration.py
+++ configuration.py
@@ -1,17 +1,19 @@
 class Config:
     #AWS Information
-    ACCESS_KEY = ''
-    SECRET_KEY = ''
-    INSTANCE_ID = ''
-    ec2_region = ""
-    ec2_amis = ['']
-    ec2_keypair = ''
-    ec2_secgroups = ['']
-    ec2_instancetype = ''
+    ACCESS_KEY = 'AKIAJI76SJZUFTLBJD2Q'
+    SECRET_KEY = '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'
+    INSTANCE_ID = 'i-08759302f6c0d4c94'
+    ec2_region = "us-west-2"
+    ec2_amis = ['ami-0e34e7b9ca0ace12d']
+    ec2_keypair = 'minecraft_keypair'
+    ec2_secgroups = ['minecraft']
+    ec2_instancetype = 't3.small'
 
     #SSH Key Path
-    SSH_KEY_FILE_PATH = ''
+    SSH_KEY_FILE_PATH = './minecraft_keypair.pem'
 
     #Server Memory Size
     #This is default to no memory specification but can be: '-Xmx1024M -Xms1024M ' (KEEP TRAILING SPACE)
     MEMORY_ALLOCATION='' 
+
+    SERVER_PASSWORD='gsm'
